859d03709abc0174de116c34c4fe0635b2db193d
romani10 <romanigamer612@gmail.com>
2024-11-20 23:26:48 +0400

    commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,41 @@
+name = "romani"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის  მიმნიჭებელი სიმბოლო
+# "romani" არის არის ცვლადისთვის მნიშვნელობა
+
+surname = "lomidze"
+
+#print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "romani" # სტრინგი არის ნებისმიერი რაღაც მოთავსებული ბრჭყალებს შორის
+age = 22 # ინტეჯერი არის ნებისმიერი მთელი რიცხვი
+height = 182.3 # ფლოათი არის ნებისმიერი ათწილადი რიცხვი
+# booLean ( booL) ტიპის ცვლადი
+
+knows_programming = True # True ან folse
+is_agly = False # snakecase( უბრალოდ წერის სტილი)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+# სტრინგი არის ბრჭყალებში მოქცეული9 სიმბოლოები
+
+"5"
+Country = "georgia"
+print(type(5))
+
+print("me mqvia romani lomidze var" + " 18 " + "wlis vcxovrob axalcixeshi")
+
+
+
+
+
+
+
+
+
+
+
+
